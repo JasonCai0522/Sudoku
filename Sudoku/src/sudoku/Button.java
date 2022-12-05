e8aa5600b66bef24296f69aa8d8c461a26a80a39
Jason Cai <jason.cai@surreyschools.ca>
2022-12-05 11:18:40 -0800

    numbers

--- Sudoku/src/sudoku/Button.java
+++ Sudoku/src/sudoku/Button.java
@@ -7,15 +7,34 @@ package sudoku;
 
 import java.awt.Dimension;
 import javax.swing.JButton;
+import javax.swing.JTextField;
+
+
 
 /**
  *
  * @author jason.cai
  */
 public class Button extends JButton{
-   public Button() {
+    int[][] numbers ={{8,4,2,9,5,6,3,1,7},
+                     {5,9,1,8,3,7,6,4,2},
+                     {6,7,3,2,4,1,8,5,9},
+                     {4,8,6,1,7,3,9,2,5},
+                     {3,1,7,5,2,9,4,8,6},
+                     {9,2,5,6,8,4,7,3,1},
+                     {2,5,4,7,6,8,1,9,3},
+                     {7,3,9,4,1,5,2,6,8},
+                     {1,6,8,3,9,2,5,7,4}
+                    };              
+    
+   public Button(int column,int row) {
+       JTextField textField = new JTextField(20);
        Dimension dmnsn = new Dimension(100,100);
        this.setPreferredSize(dmnsn);
+       int number = numbers[row][column];
+       this.setText(Integer.toString(number));
+
+       
        
    }
     
--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -1,30 +1,49 @@
 package sudoku;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 
 public class Sudoku extends JFrame{
     static int ROWS = 3;
     static int COLUMNS = 3;
     
+  
+    int[][] numbers ={{8,4,2,9,5,6,3,1,7},
+                     {5,9,1,8,3,7,6,4,2},
+                     {6,7,3,2,4,1,8,5,9},
+                     {4,8,6,1,7,3,9,2,5},
+                     {3,1,7,5,2,9,4,8,6},
+                     {9,2,5,6,8,4,7,3,1},
+                     {2,5,4,7,6,8,1,9,3},
+                     {7,3,9,4,1,5,2,6,8},
+                     {1,6,8,3,9,2,5,7,4}
+                    };                   
+            
     public Sudoku() {
         super("Sudoku");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        
         GridLayout lm = new GridLayout(ROWS,COLUMNS);
-
-
+        this.setLayout(lm);
+        
         for (int i = 0; i < ROWS; i ++){
           for (int j = 0; j < COLUMNS; j ++){
               // contains each 3x3 box of numbers
               JPanel numberPanel = new JPanel(lm);
-              System.out.println("In ");
+//              Border blackLine = BorderFactory.createLineBorder(Color.black,1);
+//              numberPanel.setBorder(blackLine);
               for (int k = 0; k < ROWS; k ++){
                 for (int l = 0; l < COLUMNS; l ++){
-                    Button b = new Button();
+                    // Finding the number
+                    int buttonColumn = 3*j + l;
+                    int buttonRow =  3*i + k;
+                    
+                    Button b = new Button(buttonColumn, buttonRow);
                     numberPanel.add(b);
                 }
               }
