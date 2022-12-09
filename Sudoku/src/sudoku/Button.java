0a511266333971380d81570de311e1dc0c2c84ee
Jason Cai <jason.cai@surreyschools.ca>
2022-12-09 13:41:18 -0800

    Added a proper grid

--- Sudoku/src/sudoku/Button.java
+++ Sudoku/src/sudoku/Button.java
@@ -5,9 +5,10 @@
  */
 package sudoku;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.JButton;
-import javax.swing.JTextField;
 
 
 
@@ -15,28 +16,42 @@ import javax.swing.JTextField;
  *
  * @author jason.cai
  */
-public class Button extends JButton{
-    int[][] numbers ={{8,4,2,9,5,6,3,1,7},
-                     {5,9,1,8,3,7,6,4,2},
-                     {6,7,3,2,4,1,8,5,9},
-                     {4,8,6,1,7,3,9,2,5},
-                     {3,1,7,5,2,9,4,8,6},
-                     {9,2,5,6,8,4,7,3,1},
-                     {2,5,4,7,6,8,1,9,3},
-                     {7,3,9,4,1,5,2,6,8},
-                     {1,6,8,3,9,2,5,7,4}
-                    };              
-    
-   public Button(int column,int row) {
-       JTextField textField = new JTextField(20);
-       Dimension dmnsn = new Dimension(100,100);
-       this.setPreferredSize(dmnsn);
-       int number = numbers[row][column];
-       this.setText(Integer.toString(number));
 
+public class Button extends JButton{
+         
+   public Button(int column,int row) {
        
+       Dimension dmnsn = new Dimension(100,100);
+       this.setPreferredSize(dmnsn);     
+       this.setBackground(Color.white);
        
-   }
-    
-    
+       Font font1 = new Font("SansSerif", Font.BOLD, 20);       
+       this.setFont(font1); 
+          
+       int number = Sudoku.numbers[row][column];
+       this.setText(Integer.toString(number));        
+   }    
 }
+
+
+
+// MIGHT HAVE REMEMBERED MRS STUSIAK SAYING TO USE TEXT FIELD BUT I DON'T REMEMBER IF WE ACTUALLY SHOULD
+//   public Button(int column,int row) {
+//
+//
+//       Dimension dmnsn = new Dimension(100,100);
+//       this.setPreferredSize(dmnsn);
+//       this.setBackground(Color.white);
+//       
+//       JTextField textField = new JTextField(10);
+//       Font font1 = new Font("SansSerif", Font.BOLD, 20);       
+//       textField.setFont(font1); 
+//       textField.setHorizontalAlignment(JTextField.CENTER);
+//       textField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+//       textField.setEditable(false);
+//
+//       
+//       
+//       int number = Sudoku.numbers[row][column];
+//       textField.setText(Integer.toString(number));
+//       this.add(textField);
--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -11,32 +11,44 @@ import javax.swing.border.Border;
 public class Sudoku extends JFrame{
     static int ROWS = 3;
     static int COLUMNS = 3;
-    
   
-    int[][] numbers ={{8,4,2,9,5,6,3,1,7},
-                     {5,9,1,8,3,7,6,4,2},
-                     {6,7,3,2,4,1,8,5,9},
-                     {4,8,6,1,7,3,9,2,5},
-                     {3,1,7,5,2,9,4,8,6},
-                     {9,2,5,6,8,4,7,3,1},
-                     {2,5,4,7,6,8,1,9,3},
-                     {7,3,9,4,1,5,2,6,8},
-                     {1,6,8,3,9,2,5,7,4}
-                    };                   
+    static int[][] numbers ={{8,4,2,9,5,6,3,1,7},
+                            {5,9,1,8,3,7,6,4,2},
+                            {6,7,3,2,4,1,8,5,9},
+                            {4,8,6,1,7,3,9,2,5},
+                            {3,1,7,5,2,9,4,8,6},
+                            {9,2,5,6,8,4,7,3,1},
+                            {2,5,4,7,6,8,1,9,3},
+                            {7,3,9,4,1,5,2,6,8},
+                            {1,6,8,3,9,2,5,7,4}
+                            };
+    
+    static boolean[][] numberGiven = {{false,false,true,false,true,true,false,true,false},
+                                      {true,true,false,false,false,true,false,false,false},
+                                      {false,false,false,false,true,false,false,false,true},
+                                      {false,true,true,true,false,false,false,false},
+                                      {false,false,true,false,false,false,true,false,false},
+                                      {false,false,false,false,true,true,true,true,false},
+                                      {true,false,false,false,true,false,false,false,false},
+                                      {false,false,false,true,false,false,false,true,true},
+                                      {false,true,false,true,true,false,true,false,false}
+                            };
             
     public Sudoku() {
         super("Sudoku");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        GridLayout lm = new GridLayout(ROWS,COLUMNS);
-        this.setLayout(lm);
+        GridLayout lm = new GridLayout(ROWS, COLUMNS);
+
+        JPanel holderPanel = new JPanel(lm);
+        
         
         for (int i = 0; i < ROWS; i ++){
           for (int j = 0; j < COLUMNS; j ++){
               // contains each 3x3 box of numbers
               JPanel numberPanel = new JPanel(lm);
-//              Border blackLine = BorderFactory.createLineBorder(Color.black,1);
-//              numberPanel.setBorder(blackLine);
+              Border blackLine = BorderFactory.createLineBorder(Color.black,1);
+              numberPanel.setBorder(blackLine);
               for (int k = 0; k < ROWS; k ++){
                 for (int l = 0; l < COLUMNS; l ++){
                     // Finding the number
@@ -47,10 +59,11 @@ public class Sudoku extends JFrame{
                     numberPanel.add(b);
                 }
               }
-              this.add(numberPanel);
+              holderPanel.add(numberPanel);
              
           }
         }
+        this.add(holderPanel);
         this.pack();
     }
     
